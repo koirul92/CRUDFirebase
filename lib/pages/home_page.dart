b42d257eafc4753fe9773ee79b53d83decc41513
koirul92 <koirulanwar98@gmail.com>
2025-05-16 15:29:30 +0700

    fix

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:curd/services/firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,10 +10,30 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //firestore
+  final FirestoreService firestoreService = FirestoreService();
+
+  final TextEditingController textController = TextEditingController();
+
   void openNoteBox() {
     showDialog(
       context: context,
-      builder: (context) => AlertDialog(content: TextField()),
+      builder:
+          (context) => AlertDialog(
+            content: TextField(controller: textController),
+            actions: [
+              ElevatedButton(
+                onPressed: () {
+                  firestoreService.addNote(textController.text);
+
+                  textController.clear();
+
+                  Navigator.pop(context);
+                },
+                child: Text("Add"),
+              ),
+            ],
+          ),
     );
   }
 
@@ -22,6 +44,36 @@ class _HomePageState extends State<HomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: openNoteBox,
         child: const Icon(Icons.add),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: firestoreService.getNotesStream(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            List notesList = snapshot.data!.docs;
+
+            return ListView.builder(
+              itemCount: notesList.length,
+              itemBuilder: (context, index) {
+                DocumentSnapshot document = notesList[index];
+                String docID = document.id;
+
+                Map<String, dynamic> data =
+                    document.data() as Map<String, dynamic>;
+                String noteText = data['note'];
+
+                return ListTile(
+                  title: Text(noteText),
+                  trailing: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.settings),
+                  ),
+                );
+              },
+            );
+          } else {
+            return const Text("No notes...");
+          }
+        },
       ),
     );
   }
--- lib/services/firestore.dart
+++ lib/services/firestore.dart
@@ -12,8 +12,20 @@ class FirestoreService {
   }
 
   // Read
+  Stream<QuerySnapshot> getNotesStream() {
+    final notesStream =
+        notes.orderBy('timestamp', descending: true).snapshots();
+
+    return notesStream;
+  }
 
   //update
+  Future<void> updateNote(String docID, String newNote) {
+    return notes.doc(docID).update({
+      'note': newNote,
+      'timestamp': Timestamp.now(),
+    });
+  }
 
   //delete
 }
